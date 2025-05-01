0ef5aa598752373812d0c59742f4c699dc0241de
Chloe Lin <u6788552@MacBook-Pro.local>
2025-05-01 10:24:24 +1000

    add script

---
+++ scripts/finetune.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#PBS -q gpuvolta
+#PBS -P um09
+#PBS -l ncpus=12
+#PBS -l ngpus=1
+#PBS -l mem=64GB
+#PBS -l walltime=00:10:00
+#PBS -l storage=scratch/um09
+#PBS -l jobfs=100GB
+#PBS -v PYTHONPATH=/scratch/um09/hl4138/llm-venv/lib/python3.10/site-packages
+
+cd /scratch/um09/hl4138
+
+module load python3/3.8.5 cuda/11.0.3
+module list
+
+source molclr-venv/bin/activate
+cd molclr
+
+# Run the Python script with the current parameters
+python3 finetune.py 
