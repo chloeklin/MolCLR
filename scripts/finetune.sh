69c9797bcbf3bcc829ee2e89f3b4233c3504b029
Han Ni Lin <hl4138@gadi-login-04.gadi.nci.org.au>
2025-05-01 12:52:42 +1000

    fix script

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -8,7 +8,7 @@
 #PBS -l walltime=00:10:00
 #PBS -l storage=scratch/um09
 #PBS -l jobfs=100GB
-#PBS -v PYTHONPATH=/scratch/um09/hl4138/llm-venv/lib/python3.10/site-packages
+#PBS -v PYTHONPATH=/scratch/um09/hl4138/molclr-venv/lib/python3.8/site-packages
 
 cd /scratch/um09/hl4138
 
@@ -16,7 +16,7 @@ module load python3/3.8.5 cuda/11.0.3
 module list
 
 source molclr-venv/bin/activate
-cd molclr
+cd MolCLR
 
 # Run the Python script with the current parameters
-python3 scripts/finetune.py 
+python3 finetune.py 
